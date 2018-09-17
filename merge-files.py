9c5f221050804911db17bd5738effd157785615c
Devin Miller <dbm79@millerha.us>
2018-09-17 15:42:23 -0400

    added open_file function/generator

--- merge-files.py
+++ merge-files.py
@@ -9,9 +9,20 @@ def get_file(folder):
     """ This is a generator function that will return one file name 
         from a folder at a time.
         Param1: string of folder to get files from
+        Yields: String file name
     """
     for file in os.listdir(folder):
         yield file
+
+
+def open_file(folder):
+    """ This is a generator function that will yield an open file object.
+        It uses the get_file generator function to get the file name to open.
+        Param1: String of folder to get files from
+        Yields: Open file object
+    """
+    for file in get_file(folder):
+        yield open(os.path.join(folder, file), "r")
 
 
 def merge_files(folder="configs/", output="configs/merged-files.txt"):
@@ -20,13 +31,12 @@ def merge_files(folder="configs/", output="configs/merged-files.txt"):
         Param1: Folder where the files are located
         Param2: Name of file with merged contents
     """
-    for file in get_file(folder):
-        with open(os.path.join(folder, file), "r") as source_file:
-            contents = source_file.read()
+    for file in open_file(folder):
+        contents = file.read()
 
-            with open(output, "a") as dest_file:
-                dest_file.write(contents)
-                dest_file.write("\n")
+        with open(output, "a") as dest_file:
+            dest_file.write(contents)
+            dest_file.write("\n")
 
     print("\n\nAll files merged to: " + output + "\n\n")
 
