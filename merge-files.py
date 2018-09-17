7e98dfba786e28d3b8142561aafefc5add8e24ee
Devin Miller <dbm79@millerha.us>
2018-09-17 14:59:21 -0400

    added generator function get_file to get list of files in folder to merge

--- merge-files.py
+++ merge-files.py
@@ -5,16 +5,22 @@
 import os, sys
 
 
+def get_file(folder):
+    """ This is a generator function that will return one file name 
+        from a folder at a time.
+        Param1: string of folder to get files from
+    """
+    for file in os.listdir(folder):
+        yield file
+
+
 def merge_files(folder="configs/", output="configs/merged-files.txt"):
     """ This will read the contents of each file in the list of file names
         and append their contents to a default file name
-        Param1: List of file names
-        Param2: Folder where the files are located
-        Param3: Name of file with merged contents
+        Param1: Folder where the files are located
+        Param2: Name of file with merged contents
     """
-    files = os.listdir(folder)
-
-    for file in files:
+    for file in get_file(folder):
         with open(os.path.join(folder, file), "r") as source_file:
             contents = source_file.read()
 
@@ -22,15 +28,15 @@ def merge_files(folder="configs/", output="configs/merged-files.txt"):
                 dest_file.write(contents)
                 dest_file.write("\n")
 
-    print("\n\nAll files merged to: " + os.path.join(folder, output) + "\n\n")
+    print("\n\nAll files merged to: " + output + "\n\n")
 
 
 def main(cmd_ln_args):
     """ This is the main program function
         Param1: sys.argv list
-    """ 
-
-    merge_files(cmd_ln_args[1])
+    """
+    output_file = cmd_ln_args[1] + "-merged.txt"
+    merge_files(cmd_ln_args[1], output_file)
 
 
 if __name__ == "__main__":
